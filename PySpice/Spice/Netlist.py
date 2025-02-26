19950009086fc664213706840a6f8968699f78b2
“Alihossein <alih.sepahvand@gmail.com>
2025-02-26 13:23:42 -0700

    enhance add_current_probe and add_esr methods to accept optional name parameter

--- PySpice/Spice/Netlist.py
+++ PySpice/Spice/Netlist.py
@@ -297,7 +297,7 @@ class Pin(PinDefinition):
 
     ##############################################
 
-    def add_current_probe(self, circuit):
+    def add_current_probe(self, circuit, name=None):
 
         """Add a current probe between the node and the pin.
 
@@ -308,9 +308,29 @@ class Pin(PinDefinition):
         # Fixme: require a reference to circuit
         # Fixme: add it to a list
 
+
         node = self._node
-        self._node = '_'.join((self._element.name, self._name))
-        circuit.V(self._node, node, self._node, '0')
+        self._node = '_'.join((self._element.name, str(node),str(self.position) ))
+        if name is None:
+            name = self._node
+        return circuit.V(name, node, self._node, '0')
+    
+    def add_esr(self, circuit, name= None, value=1e-6):
+
+        """Add a series resistance between the node and the pin.
+
+        The ammeter is named *ElementName_PinName*.
+
+        """
+
+        # Fixme: require a reference to circuit
+        # Fixme: add it to a list
+        self
+        node = self._node
+        self._node = '_'.join((self._element.name,  str(self._node), str(self.position)))
+        if name is None:
+            name = self._node
+        return circuit.R(name, node, self._node, value)
 
 ####################################################################################################
 
